15ebf6fafdfe284c9918310ebe7e9849c2e684f9
chris <chris>
2016-02-14 11:52:09 +0100

    modified Makefile modified main.c

---
+++ projects/timer/main.c
@@ -0,0 +1,23 @@
+// #include "stm32f4xx.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+
+int main(int argc, char const *argv[]) {
+  SystemInit();
+
+  {
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	GPIO_InitTypeDef GPIO_initStruct;
+
+	GPIO_initStruct.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_initStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_initStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_initStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_initStruct.GPIO_Speed = GPIO_Speed_50MHz;
+
+	GPIO_Init(GPIOD, &GPIO_initStruct);
+	}
+
+  return 0;
+}
